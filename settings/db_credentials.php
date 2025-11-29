1269351b2a16dce384387eeefdffa1f298b564ab
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2025-11-29 03:22:46 +0000

    server cred

--- settings/db_credentials.php
+++ settings/db_credentials.php
@@ -9,39 +9,39 @@
 
 // server
 
-// if(!defined("SERVER")){
-//     define("SERVER", "localhost");
-// }
-
-// if(!defined('USERNAME')){
-//     define('USERNAME', 'reindorf.narh');
-// }
-
-// if(!defined('PASSWD')){
-//     define('PASSWD', 'Qwaszx123.');
-// }
-
-// if(!defined('DB_NAME')){
-//     define('DB_NAME', 'ecommerce_2025A_reindorf_narh');
-// }
-
-
-
-// local host
 if(!defined("SERVER")){
     define("SERVER", "localhost");
 }
 
 if(!defined('USERNAME')){
-    define('USERNAME', 'root');
+    define('USERNAME', 'reindorf.narh');
 }
 
 if(!defined('PASSWD')){
-    define('PASSWD', '');
+    define('PASSWD', 'Qwaszx123.');
 }
 
 if(!defined('DB_NAME')){
-    define('DB_NAME', 'shopping');
+    define('DB_NAME', 'ecommerce_2025A_reindorf_narh');
 }
+
+
+
+// local host
+// if(!defined("SERVER")){
+//     define("SERVER", "localhost");
+// }
+
+// if(!defined('USERNAME')){
+//     define('USERNAME', 'root');
+// }
+
+// if(!defined('PASSWD')){
+//     define('PASSWD', '');
+// }
+
+// if(!defined('DB_NAME')){
+//     define('DB_NAME', 'shopping');
+// }
 
 ?>
